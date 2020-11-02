3cc45b1b2489accc91dfdc7231ee01cf84c70499
김효성 <devvenusk@gmail.com>
2020-11-02 13:04:38 +0900

    STT Example Project 생성

---
+++ STTExample/STTExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STTExample
+//
+//  Created by 김효성 on 2020/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
